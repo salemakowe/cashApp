61a551cd96cc08826c6b679f10fa238ebd3728b5
Salem Akowe <salemakowe@gmail.com>
2023-04-20 02:03:11 +0100

    .

---
+++ cashapp/lib/pages/create_account.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class CreateAccoutPage extends StatefulWidget {
+  const CreateAccoutPage({super.key});
+
+  @override
+  State<CreateAccoutPage> createState() => _CreateAccoutPageState();
+}
+
+class _CreateAccoutPageState extends State<CreateAccoutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Create an account",
+          style: TextStyle(
+            color: Colors.black,
+            fontFamily: 'Montserrat',
+            fontSize: 18,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.arrow_back_ios_outlined,
+            size: 20.0,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(24, 16, 24, 10),
+        child: Column(
+          children: const [
+            Text(
+              "Cashwise would like to know your phone number",
+              style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Kindly enter your phone number below as a verification code will be sent to it",
+              style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 13,
+                  fontWeight: FontWeight.normal),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ cashapp/lib/pages/otp_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OtpPage extends StatefulWidget {
+  const OtpPage({super.key});
+
+  @override
+  State<OtpPage> createState() => _OtpPageState();
+}
+
+class _OtpPageState extends State<OtpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
