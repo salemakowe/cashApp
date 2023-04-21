86e5bb35290f333d6ef4650b31dd3f07e118d04a
Salem Akowe <salemakowe@gmail.com>
2023-04-21 08:08:57 +0100

    .

---
+++ cashapp/lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Kolors {
+  static const Color textGreen = Color(0xff17C373);
+  static const Color subText1 = Color(0xff242424);
+  static const Color subText2 = Color(0xff4E4E4E);
+  static const Color subText3 = Color(0xff6C727F);
+  static const Color debitText = Color(0xffFF0000);
+  static const Color creditText = Color(0xff17C373);
+}
--- cashapp/lib/pages/onboarding.dart
+++ cashapp/lib/pages/onboarding.dart
@@ -10,6 +10,6 @@ class OnboardingPage extends StatefulWidget {
 class _OnboardingPageState extends State<OnboardingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
